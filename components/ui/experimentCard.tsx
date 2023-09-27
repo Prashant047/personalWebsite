99d467b725272f18ca3a4139c21b1e1c89c416c2
Prashant047 <prash.kumar047@gmail.com>
2023-09-27 20:11:26 +0100

    modified experiment card and added repolink icon

--- components/ui/experimentCard.tsx
+++ components/ui/experimentCard.tsx
@@ -1,4 +1,4 @@
-import { AlignLeft, ArrowRight } from "lucide-react"
+import { AlignLeft, ArrowRight, Github } from "lucide-react"
 import { TagFramerMotion, TagNext, TagReact, TagTailWind } from "./tag"
 import { cn } from "@/lib/utils"
 import * as React from "react"
@@ -12,7 +12,7 @@ const tagMapping = {
   'framermotion': <TagFramerMotion/>,
 }
  
-export interface ExperimentCardProps extends React.HTMLAttributes<HTMLAnchorElement> {
+export interface ExperimentCardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string,
   description: string,
   href: string
@@ -20,32 +20,37 @@ export interface ExperimentCardProps extends React.HTMLAttributes<HTMLAnchorElem
 }
  
 const ExperimentCard = React.forwardRef<
-  HTMLAnchorElement,
+  HTMLDivElement,
   ExperimentCardProps
 >(({ className, href, title, description, tags ,...props }, ref) => (
-    <a href={href} ref={ref} className={cn(className)} {...props} >
-      <Card className="group hover:bg-neutral-800/50 border-neutral-800">
-        <CardHeader className="flex items-center gap-2">
-          <i>
-            <AlignLeft className="text-neutral-400 text-blue-700" size={15}/>
-          </i>
+    <Card ref={ref} className={cn("group hover:bg-neutral-800/50 border-neutral-800", className)} {...props} >
+      <CardHeader className="flex items-center gap-2">
+        <i>
+          <AlignLeft className="text-neutral-400 text-blue-700" size={15}/>
+        </i>
+        <a href={href} className="hover:underline">
           <CardTitle>
             {title}
           </CardTitle>
-          <i>
-            <ArrowRight className="text-neutral-400 group-hover:translate-x-3 group-hover:text-neutral-300 transition" size={15}/>
-          </i>
-        </CardHeader>
-        <CardContent className="pt-2">
-          <p className="text-neutral-500 group-hover:text-neutral-400 transition">
-            {description}
-          </p>
-        </CardContent>
-        <CardFooter className="flex items-center gap-2">
-          {tags.map((tag:TagType) => tagMapping[tag] )}
-        </CardFooter>
-      </Card>
-    </a>
+        </a>
+        <i>
+          <ArrowRight className="text-neutral-400 group-hover:translate-x-3 group-hover:text-neutral-300 transition" size={15}/>
+        </i>
+        <i className="ml-auto h-6 w-6 text-neutral-400 rounded-full bg-neutral-800/50 hover:bg-neutral-800 hover:text-neutral-200 flex items-center justify-center">
+          <a href="https://github.com/Prashant047">
+            <Github className="transition" size={13}/>
+          </a>
+        </i>
+      </CardHeader>
+      <CardContent className="pt-2">
+        <p className="text-neutral-500 group-hover:text-neutral-400 transition">
+          {description}
+        </p>
+      </CardContent>
+      <CardFooter className="flex items-center gap-2">
+        {tags.map((tag:TagType) => tagMapping[tag] )}
+      </CardFooter>
+    </Card>
 ))
 ExperimentCard.displayName = "ExperimentCard"
 
